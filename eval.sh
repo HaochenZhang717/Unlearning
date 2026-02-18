2d86d64dc078010c3dbc3c77d68c80d43beb0a57
Haochen Zhang <hz112@rice.edu>
2026-02-18 16:10:36 -0600

    0218

--- eval.sh
+++ eval.sh
@@ -1,7 +1,14 @@
+
+export CUDA_VISIBLE_DEVICES=7
+
+FORGET_RATIO=5
+METHOD="NPO"
+CACHE_PATH="/playpen-shared/haochenz/UMU-Bench-result/ckpts/llava_${METHOD}_forget${FORGET_RATIO}"
+OUT_PATH="/playpen-shared/haochenz/UMU-Bench-result/results"
 python eval.py \
-  --model_id llava-hf/llava-1.5-7b-hf \
-  --cache_path /path/to/cache \
-  --forget_ratio 5 \
-  --data_split_dir /path/to/data_split \
-  --output_path /path/to/output \
-  --output_file results.json
+  --model_id "llava-hf/llava-1.5-7b-hf" \
+  --cache_path ${CACHE_PATH} \
+  --forget_ratio ${FORGET_RATIO} \
+  --data_split_dir "/playpen-shared/haochenz/UMU-Bench" \
+  --output_path ${OUT_PATH} \
+  --output_file "${METHOD}_forget${FORGET_RATIO}.json"
