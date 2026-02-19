31bf7cbf2cf95a8ceda423f250828760daba6ce0
Haochen Zhang <hz112@rice.edu>
2026-02-19 13:39:49 -0600

    0219

--- finetune/finetune.sh
+++ finetune/finetune.sh
@@ -8,8 +8,8 @@ export HF_DATASETS_CACHE=/playpen-shared/haochenz/hf_cache
 export WANDB_DIR=/playpen-shared/haochenz/wandb
 export WANDB_CACHE_DIR=/playpen-shared/haochenz/wandb_cache
 
-BATCH_SIZE=2
-GRAD_ACC=4
+BATCH_SIZE=8
+GRAD_ACC=1
 NUM_GPUS=2
 LR=2e-5
 NUM_EPOCHS=5
