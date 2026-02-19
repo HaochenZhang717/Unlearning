bdc0f4e074f1938a4b77af7972d8e6bc677ae021
Haochen Zhang <hz112@rice.edu>
2026-02-19 11:56:01 -0600

    0219

--- finetune/finetune.sh
+++ finetune/finetune.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 set -e
 
-# ===========================
-# Config
-# ===========================
-
 BATCH_SIZE=2
+GRAD_ACC=8
 LR=2e-5
 NUM_EPOCHS=5
 MAX_LENGTH=384
 
 MODEL_ID="llava-hf/llava-1.5-7b-hf"
 DATA_DIR="/playpen-shared/haochenz/UMU-Bench/full_data/train-00000-of-00001.parquet"
-effective_bs=BATCH_SIZE * 8
-SAVE_DIR="/playpen-shared/haochenz/UMU-Bench-result/ckpts/finetuned_llava_fullset_lr${LR}_bs${effective_bs}"
 
+EFFECTIVE_BS=$((BATCH_SIZE * GRAD_ACC))
 
+SAVE_DIR="/playpen-shared/haochenz/UMU-Bench-result/ckpts/finetuned_llava_fullset_lr${LR}_effbs${EFFECTIVE_BS}"
 
 mkdir -p /playpen-shared/haochenz/hf_cache
 
@@ -27,25 +24,20 @@ export WANDB_CACHE_DIR=/playpen-shared/haochenz/wandb_cache
 
 export CUDA_VISIBLE_DEVICES=4
 
-# ===========================
-# Run
-# ===========================
+echo "Effective batch size = ${EFFECTIVE_BS}"
 echo "Running finetuning..."
-echo "MODEL_ID = ${MODEL_ID}"
-echo "DATA_DIR = ${DATA_DIR}"
-echo "SAVE_DIR = ${SAVE_DIR}"
 
 accelerate launch \
   --num_processes 1 \
   --num_machines 1 \
   --mixed_precision bf16 \
   --dynamo_backend no \
-  --gradient_accumulation_steps 8 \
   finetune.py \
   --model_id ${MODEL_ID} \
   --save_dir ${SAVE_DIR} \
   --data_dir ${DATA_DIR} \
   --batch_size ${BATCH_SIZE} \
+  --gradient_accumulation_steps ${GRAD_ACC} \
   --lr ${LR} \
   --num_epochs ${NUM_EPOCHS} \
   --max_length ${MAX_LENGTH}
