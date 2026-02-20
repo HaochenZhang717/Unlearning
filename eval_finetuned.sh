0b9353d74bd050fc6ac833a7a81ca52e3509ff75
Haochen Zhang <hz112@rice.edu>
2026-02-19 18:29:56 -0600

    0219

--- eval_finetuned.sh
+++ eval_finetuned.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=7
 
 FORGET_RATIOS=(5)
 
@@ -12,8 +12,9 @@ MODEL_ID="llava-hf/llava-1.5-7b-hf"
 mkdir -p ${OUT_PATH}
 
 for FORGET_RATIO in "${FORGET_RATIOS[@]}"; do
-    CACHE_PATH="/playpen-shared/haochenz/UMU-Bench-result/ckpts/finetuned_llava_fullset"
-    OUTPUT_FILE="fintuned.json"
+#    CACHE_PATH="/playpen-shared/haochenz/UMU-Bench-result/ckpts/finetuned_llava_fullset"
+    CACHE_PATH="/playpen-shared/haochenz/UMU-Bench-result/ckpts/finetuned_llava_fullset_lr2e-5_effbs16"
+    OUTPUT_FILE="fintuned_lr2e-5_effbs16.json"
 
     echo "========================================="
     echo "CACHE_PATH=${CACHE_PATH}"
